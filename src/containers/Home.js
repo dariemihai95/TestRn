e2126a14dd02b6e9f5b19dbc7d5b508aff7f33d0
Mihai Darie <dariemihai95@gmail.com>
2021-02-20 13:32:18 +0200

    created cards, changed styling, updated cards data, added loading

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -18,46 +18,61 @@ import {
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { fetchData } from '../services/fetch';
 import { urls } from '../utils/constants';
+import { Card } from 'react-native-elements';
 
 export default class Home extends React.Component {
 
   state = {
-    dataList: []
+    dataList: [],
+    isLoading: false,
   }
 
   setData = async () => {
+    this.setState({ isLoading: true })
     const payload = await fetchData(urls);
-    this.setState({ dataList: payload });
+    this.setState({ dataList: payload, isLoading: false });
   }
 
   clearData = () => {
-    this.setState({dataList: []});
+    this.setState({ dataList: [], isLoading: false });
   }
 
   render() {
 
     const {
       setData, clearData,
-      state: { dataList }
+      state: { dataList, isLoading }
     } = this;
 
     return (
-      <>
-        <View>
-          <TouchableOpacity onPress={setData}><Text>Get</Text></TouchableOpacity>
-          <TouchableOpacity onPress={clearData}><Text>Clear</Text></TouchableOpacity>
+      <>{isLoading
+        ? <View style={styles.loadingContainer}><Text>Loading...</Text></View>
+        : (
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}
+          >
+            {dataList.map((dataItem, index) => {
+              return (
+                <View key={index}>
+                  <Card containerStyle={{ padding: 5, borderRadius: 5 }} >
+                    <Text style={styles.cardText}>title: {dataItem.title}</Text>
+                    <Text style={styles.cardText}>artist: {dataItem.artist}</Text>
+                    <Text style={styles.cardText}>label: {dataItem.label}</Text>
+                    <Text style={styles.cardText}>year: {dataItem.year}</Text>
+                  </Card>
+                </View>
+              )
+            })}
+            <View style={{ marginBottom: 70 }} />
+          </ScrollView>)
+      }
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonsBar}>
+            <TouchableOpacity style={{ ...styles.button, backgroundColor: '#95e1d3' }} onPress={setData}><Text>Get</Text></TouchableOpacity>
+            <TouchableOpacity style={{ ...styles.button, backgroundColor: '#f38181' }} onPress={clearData}><Text>Clear</Text></TouchableOpacity>
+          </View>
         </View>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          {dataList.map((dataItem, index) => {
-            return (
-              <View key={index}>
-                <Text>{dataItem.artist}</Text>
-              </View>
-            )
-          })}
-        </ScrollView>
       </>
     );
   };
@@ -66,5 +81,33 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
+    marginBottom: 20,
+    height: '100%'
   },
+  button: {
+    margin: 20,
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 15,
+  },
+  cardText: {
+    marginLeft: 5
+  },
+  loadingContainer: {
+    backgroundColor: Colors.lighter,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    marginBottom: 20
+  },
+  buttonsContainer: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 20
+  },
+  buttonsBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
